c1032c1b1114cf12eff1728b3defa98d7ff53474
safin-sys <mdsafinofficial@gmail.com>
2021-07-04 22:44:01 +0600

    nav done

---
+++ pages/_document.js
@@ -0,0 +1,22 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+                    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet" />
+                    <link rel="shortcut icon" href="favicon.svg" type="image/x-icon" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
+
+export default MyDocument
---
+++ src/components/Nav.jsx
@@ -0,0 +1,66 @@
+import { Container, Flex, Link } from "@chakra-ui/layout"
+import { Avatar, Heading, Switch } from "@chakra-ui/react"
+import { useColorMode } from "@chakra-ui/color-mode"
+import { Menu, MenuButton, MenuList, MenuDivider } from "@chakra-ui/react"
+import { FormControl, FormLabel } from "@chakra-ui/form-control"
+
+export const Nav = () => {
+    return (
+        <Container h="4rem" maxW="container.lg">
+            <Flex h="100%" alignItems="center" justifyContent="space-between">
+                <Heading as="h3" fontSize="1.5rem" fontWeight="normal" fontFamily="Pacifico">Tourny</Heading>
+                <NavLinks />
+            </Flex>
+        </Container>
+    )
+}
+
+const NavLinks = () => {
+    const { colorMode, toggleColorMode } = useColorMode()
+    const isLoggedIn = true
+    return (
+        <Flex alignItems="center" justifyContent="space-between" fontWeight="medium">
+            {isLoggedIn ? <LoggedInLinks /> : <LoggedOutLinks />}
+            <Menu closeOnSelect={false}>
+                <MenuButton colorScheme="blue">
+                    <Avatar name="SafinTheShip" src="https://live.staticflickr.com/2445/3610341613_c8434baab9_b.jpg" />
+                </MenuButton>
+                <MenuList minWidth="240px">
+                    <Container display="flex" flexDirection="column">
+                        <Link>SafinTheShip</Link>
+                        <Link>Your Matches</Link>
+                        <MenuDivider />
+                        <FormControl display="flex" alignItems="center">
+                            <FormLabel>
+                                Toggle {colorMode === "dark" ? "Light" : "Dark"}
+                            </FormLabel>
+                            <Switch onChange={toggleColorMode} />
+                        </FormControl>
+                        <MenuDivider />
+                        <Link>Logout</Link>
+                    </Container>
+                </MenuList>
+            </Menu>
+        </Flex>
+    )
+}
+
+const LoggedInLinks = () => {
+    return (
+        <>
+            <Link mr="1rem">Teams</Link>
+            <Link mr="1rem">Players</Link>
+            <Link mr="2rem">TW Cup 2021</Link>
+        </>
+    )
+}
+
+const LoggedOutLinks = () => {
+    return (
+        <>
+            <Link mr="1rem">TW Cup 2021</Link>
+            <Link mr="1rem">Login</Link>
+            <Link mr="2rem">Register</Link>
+        </>
+    )
+}
