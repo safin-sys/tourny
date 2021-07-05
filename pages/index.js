017bdfa710112a9fc78ba277ec743c6f7e2d8b20
safin-sys <mdsafinofficial@gmail.com>
2021-07-06 01:32:14 +0600

    players

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import { Nav } from '../src/components/Nav'
 import { MVP } from '../src/components/MVP'
 import { Ongoing } from '../src/components/Ongoing';
+import { Footer } from '../src/components/Footer';
+import { Players } from '../src/components/Players';
 
 const MVPInfo = { 
 	playerName: 'SafinTheShip',
@@ -28,6 +30,8 @@ export default function Home() {
 			<Nav />
 			<MVP MVPInfo={MVPInfo} />
 			<Ongoing OngoingInfo={OngoingInfo} />
+			<Players />
+			<Footer />
 		</>
 	)
 }
