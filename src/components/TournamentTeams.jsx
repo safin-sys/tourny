848862e5ce7518271eba3657057f8ac5f9b2d17f
safin-sys <mdsafinofficial@gmail.com>
2021-07-17 01:07:48 +0600

    clean up

--- src/components/TournamentTeams.jsx
+++ src/components/TournamentTeams.jsx
@@ -1,8 +1,7 @@
-import { Button, Container, Flex, Grid, useDisclosure } from "@chakra-ui/react"
+import { Grid } from "@chakra-ui/react"
 import { Team } from "./AvatarContainer"
 
 export const TournamentTeams = ({ teams }) => {
-    const { isOpen, onOpen, onClose } = useDisclosure()
     return (
         <Grid templateColumns="1fr 1fr" gap="1.5rem">
             {teams.map((stand, i) => {
