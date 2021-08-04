40236122a4ff88c0fe28e3c905c9c2cc383c16bb
safin-sys <mdsafinofficial@gmail.com>
2021-08-04 14:30:27 +0600

    favicon fix

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ class MyDocument extends Document {
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                     <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet" />
-                    <link rel="shortcut icon" href="favicon.svg" type="image/x-icon" />
+                    <link rel="icon" href="../favicon.svg" type="image/x-icon" />
                 </Head>
                 <body>
                     <Main />
