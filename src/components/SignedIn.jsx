50332af1ac2d5c2ddbcb2d5fd70d2bdd8118b477
safin-sys <mdsafinofficial@gmail.com>
2021-08-10 00:35:39 +0600

    react weird

--- src/components/SignedIn.jsx
+++ src/components/SignedIn.jsx
@@ -5,7 +5,7 @@ export default function SingedIn() {
         <Container maxW="75vw" display="flex" alignItems="center" h="calc(100vh - 128px)">
             <Box mx="auto">
                 <Heading>You are already signed in.</Heading>
-                <Text mt={4}>Why are you still here? Just to suffer? Every night, You can feel your leg… and your arm… even your fingers. The body You've lost… the comrades You've lost… won’t stop hurting… It’s like they’re all still there. You feel it, too, don’t you?</Text>
+                <Text mt={4}>{"Why are you still here? Just to suffer? Every night, You can feel your leg… and your arm… even your fingers. The body You've lost… the comrades You've lost… won’t stop hurting… It’s like they’re all still there. You feel it, too, don’t you?"}</Text>
             </Box>
         </Container>
     )
