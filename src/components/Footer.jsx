4dfaf9db85df0aa14274caa82c3bef00dae706bc
safin-sys <mdsafinofficial@gmail.com>
2021-08-13 20:17:17 +0600

    footer changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,10 @@
-import { Flex, Text, Link, Container } from "@chakra-ui/layout"
+import { Flex, Link, Container } from "@chakra-ui/layout"
 
 export const Footer = () => {
     return (
         <Container maxW="75vw">
             <Flex as="footer" justifyContent="space-between" alignItems="center" h="4rem">
-                <Text>Designed and Coded by <Link isExternal href="https://safin.netlify.app/"  fontStyle="italic">Safin Ahmed</Link></Text>
+                <Link isExternal href="https://safin.netlify.app/"  fontStyle="italic">Safin Ahmed</Link>
                 <Link isExternal href="mailto:safinahmed248@gmail.com" fontStyle="italic">safinahmed248@gmail.com</Link>
             </Flex>
         </Container>
