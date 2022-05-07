720929ad095f9ec26f1fb0bc6be9e362110bf642
safin-sys <mdsafinofficial@gmail.com>
2022-05-08 00:02:30 +0600

    FEAT(player): loading skeleton added

--- src/components/Players/PlayerHeader.jsx
+++ src/components/Players/PlayerHeader.jsx
@@ -5,49 +5,59 @@ import {
     Button,
     Flex,
     Heading,
+    Skeleton,
 } from "@chakra-ui/react";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 const PlayerHeader = ({ player }) => {
-    const { name, email, profilePicture, coverPicture,  phoneNumber } =
-        player ? player : {};
+    const { name, email, profilePicture, coverPicture, phoneNumber } = player
+        ? player
+        : {};
     return (
         <Box>
             <Flex flexDir="column" pos="relative">
-                <AspectRatio
-                    pos="relative"
-                    ratio={2.2 / 1}
-                    w="100%"
-                    h="150px"
-                    borderRadius="8px"
-                    overflow="hidden"
-                >
-                    <Image
-                        src={
-                            coverPicture
-                                ? coverPicture
-                                : "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Lucian_6.jpg"
-                        }
-                        alt="Lucian"
-                        layout="fill"
-                        objectFit="cover"
-                        objectPosition="0 0"
-                        priority
-                    />
-                </AspectRatio>
-                <Avatar
+                <Skeleton isLoaded={coverPicture || coverPicture === null}>
+                    <AspectRatio
+                        pos="relative"
+                        ratio={2.2 / 1}
+                        w="100%"
+                        h="150px"
+                        borderRadius="8px"
+                        overflow="hidden"
+                    >
+                        <Image
+                            src={
+                                coverPicture
+                                    ? coverPicture
+                                    : "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Lucian_6.jpg"
+                            }
+                            alt="Lucian"
+                            layout="fill"
+                            objectFit="cover"
+                            objectPosition="0 0"
+                            priority
+                        />
+                    </AspectRatio>
+                </Skeleton>
+                <Skeleton
+                    isLoaded={profilePicture || profilePicture === null}
                     pos="absolute"
                     top="54%"
-                    size="xl"
-                    name="Faker"
                     ml="1rem"
-                    border="2px solid #3182ce"
-                    src={
-                        profilePicture
-                            ? profilePicture
-                            : "https://ddragon.leagueoflegends.com/cdn/12.8.1/img/profileicon/0.png"
-                    }
-                />
+                    borderRadius="full"
+                >
+                    <Avatar
+                        size="xl"
+                        name={name}
+                        border="2px solid #3182ce"
+                        src={
+                            profilePicture
+                                ? profilePicture
+                                : "https://ddragon.leagueoflegends.com/cdn/12.8.1/img/profileicon/0.png"
+                        }
+                    />
+                </Skeleton>
                 <Button
                     m="1rem 0 0 auto"
                     variant="outline"
@@ -61,23 +71,27 @@ const PlayerHeader = ({ player }) => {
                 </Button>
             </Flex>
             <Box m="1.5rem 0 0 1rem">
-                <Flex alignItems="center" gap=".25rem">
-                    <Heading fontSize="1rem">{name}</Heading>
-                    <Heading
-                        fontSize="1rem"
-                        h="5px"
-                        lineHeight="0"
-                        color="gray.500"
-                    >
-                        .
+                <Skeleton isLoaded={name}>
+                    <Flex alignItems="center" gap=".25rem">
+                        <Heading fontSize="1rem">{name}</Heading>
+                        <Heading
+                            fontSize="1rem"
+                            h="5px"
+                            lineHeight="0"
+                            color="gray.500"
+                        >
+                            .
+                        </Heading>
+                        <Heading fontSize="1rem" color="gray.500">
+                            Player
+                        </Heading>
+                    </Flex>
+                </Skeleton>
+                <Skeleton isLoaded={email}>
+                    <Heading fontSize="1rem" color="gray.500" mt=".25rem">
+                        {email} {phoneNumber ? `| ${phoneNumber}` : null}
                     </Heading>
-                    <Heading fontSize="1rem" color="gray.500">
-                        Player
-                    </Heading>
-                </Flex>
-                <Heading fontSize="1rem" color="gray.500" mt=".25rem">
-                    {email} {phoneNumber ? `| ${phoneNumber}` : null}
-                </Heading>
+                </Skeleton>
             </Box>
         </Box>
     );
