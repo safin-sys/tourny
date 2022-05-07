ad7e90c1034f05cc0ee3632e7d62591173747345
safin-sys <mdsafinofficial@gmail.com>
2022-05-07 23:43:43 +0600

    FEAT(404): custom 404 page added

---
+++ pages/404.js
@@ -0,0 +1,26 @@
+import { Container, Flex, Grid, Text } from "@chakra-ui/react";
+import Nav from "../src/components/shared/Nav";
+import Footer from "../src/components/shared/Footer";
+import Image from "next/image";
+
+NotFound.title = "404 | Tourny";
+export default function NotFound() {
+    return (
+        <Container>
+            <Nav />
+            <Grid h="calc(100vh - 12rem)" placeContent="center">
+                <Flex flexDir="column" alignItems="center">
+                    <Image
+                        src="/images/404.webp"
+                        height="100px"
+                        width="100px"
+                        alt="404"
+                        priority
+                    />
+                    <Text fontWeight="bold" mt="1rem">404 | Does Not Compute</Text>
+                </Flex>
+            </Grid>
+            <Footer />
+        </Container>
+    )
+}
