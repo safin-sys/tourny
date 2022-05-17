646ab9033c6e6b1955fa6807468c42d21dfebb42
safin-sys <mdsafinofficial@gmail.com>
2022-05-17 23:04:23 +0600

    FIX(API): api is now https

--- src/libs/useChampionSkin.js
+++ src/libs/useChampionSkin.js
@@ -3,7 +3,7 @@ import useSWR from "swr";
 const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 export default function useChampionSkin(champion) {
-    const { data, error } = useSWR(champion ? `http://ddragon.leagueoflegends.com/cdn/12.8.1/data/en_US/champion/${champion}.json` : null, fetcher);
+    const { data, error } = useSWR(champion ? `https://ddragon.leagueoflegends.com/cdn/12.8.1/data/en_US/champion/${champion}.json` : null, fetcher);
     return {
         skins: data?.data[champion]?.skins,
         isLoading: !error && !data,
